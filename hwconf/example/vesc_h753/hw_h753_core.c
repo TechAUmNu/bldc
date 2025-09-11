81a212c9090afd25cdcf8b219f955c0af944e7bf
Euan <euan.mutch@gmail.com>
2025-09-12 00:17:30 +0100

    Remove unused bits and correct comment

--- hwconf/example/vesc_h753/hw_h753_core.c
+++ hwconf/example/vesc_h753/hw_h753_core.c
@@ -136,19 +136,8 @@ void hw_init_gpio(void) {
 //	rccEnableDAC1(TRUE);
 //	DAC1->CR |= DAC_CR_EN1;
 //	DAC1->DHR12R1 = 2047;
-
-	terminal_register_command_callback(
-			"shutdown",
-			"Shutdown VESC now.",
-			0,
-			terminal_shutdown_now);
-
-	terminal_register_command_callback(
-			"test_button",
-			"Try sampling the shutdown button",
-			0,
-			terminal_button_test);
 }
+
 // TODO EM: need to work out the correct sample times
 void hw_setup_adc_channels(void) {
 	// ADC1 regular channels
@@ -316,41 +305,3 @@ void hw_try_restore_i2c(void) {
 	}
 }
 
-bool hw_sample_shutdown_button(void) {
-	chMtxLock(&shutdown_mutex);
-
-	bt_diff = 0.0;
-
-	for (int i = 0;i < 3;i++) {
-		palSetPadMode(HW_SHUTDOWN_GPIO, HW_SHUTDOWN_PIN, PAL_MODE_INPUT_ANALOG);
-		chThdSleep(5);
-		float val1 = ADC_VOLTS(ADC_IND_SHUTDOWN);
-		chThdSleepMilliseconds(1);
-		float val2 = ADC_VOLTS(ADC_IND_SHUTDOWN);
-		palSetPadMode(HW_SHUTDOWN_GPIO, HW_SHUTDOWN_PIN, PAL_MODE_OUTPUT_PUSHPULL);
-		chThdSleepMilliseconds(1);
-
-		bt_diff += (val1 - val2);
-	}
-
-	chMtxUnlock(&shutdown_mutex);
-
-	return (bt_diff > 0.12);
-}
-
-static void terminal_shutdown_now(int argc, const char **argv) {
-	(void)argc;
-	(void)argv;
-	DISABLE_GATE();
-	HW_SHUTDOWN_HOLD_OFF();
-}
-
-static void terminal_button_test(int argc, const char **argv) {
-	(void)argc;
-	(void)argv;
-
-	for (int i = 0;i < 40;i++) {
-		commands_printf("BT: %d %.2f", HW_SAMPLE_SHUTDOWN(), (double)bt_diff);
-		chThdSleepMilliseconds(100);
-	}
-}
